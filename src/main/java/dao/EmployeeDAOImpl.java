d06f56aaf06837ce5cd08cd4483e8754d41ea19e
Qwaieare <gb-grand@mail.ru>
2023-05-24 21:39:14 +0300

    hw20230520_Hibernate is done

---
+++ src/main/java/dao/EmployeeDAOImpl.java
@@ -0,0 +1,47 @@
+package dao;
+import config.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import models.Employee;
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+    @Override
+    public void create(Employee employee) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.save(employee);
+            transaction.commit();
+        }
+    }
+    @Override
+    public Employee readById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Employee.class, id);
+    }
+
+    @Override
+    public List<Employee> readAll() {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("From Employee").list();
+    }
+
+    @Override
+    public void updateEmployee(Employee employee) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+             session.update(employee);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void delete(Employee employee) {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(employee);
+            transaction.commit();
+        }
+    }
+
+
+}
